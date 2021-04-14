53adec8ab2e255b90d386f5f9a63657e7a7b5d8c
Rodrigo Correia <rodrigocorrei@gmail.com>
2021-04-14 20:41:43 -0300

    My Firt Module

---
+++ MyAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    //My Components
+  ],
+  imports: [
+    CommonModule
+    //my Modules
+  ],
+  providers:[
+    // My Services
+  ]
+})
+export class FuncionalidadeModule { }
